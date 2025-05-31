07e3e080b9af84e301124ce7546d0a76793e22d5
susmnty <techcode1208@gmail.com>
2025-05-31 21:08:44 +0530

    Added Given two integers A and B

---
+++ Given two integers A and B. Your task is to return their product.py
@@ -0,0 +1,7 @@
+# Given two integers A and B. Your task is to return their product.
+
+#User function Template for python3
+class Solution:
+    def multiplication (ob,A,B):
+        # code here 
+        return A * B
