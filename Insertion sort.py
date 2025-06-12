8fc1db4d43427c23b74374164682bafd90a6e43e
susmnty <techcode1208@gmail.com>
2025-06-12 21:10:08 +0530

    Insertion sort

---
+++ Insertion sort.py
@@ -0,0 +1,11 @@
+# Please change the array in-place
+class Solution:
+    def insertionSort(self, arr):
+        #code here
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j>= 0 and arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
