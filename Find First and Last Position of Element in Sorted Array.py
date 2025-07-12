fbb491eb014cef714223d2c6a56748bf47726838
susmnty <techcode1208@gmail.com>
2025-07-12 12:38:10 +0530

    initial commit

---
+++ Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,33 @@
+from typing import List 
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findFirst(nums, target):
+            left, right = 0, len(nums) - 1
+            first = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    first = mid
+                    right = mid - 1  # Keep searching left part
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return first
+
+        def findLast(nums, target):
+            left, right = 0, len(nums) - 1
+            last = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    last = mid
+                    left = mid + 1  # Keep searching right part
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return last
+
+        return [findFirst(nums, target), findLast(nums, target)]
